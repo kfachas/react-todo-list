d572e652d022558da368826a6dc332ae301cf0b5
Kevin <kfachas@gmail.com>
2021-07-23 18:44:02 +0200

    test#1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [values, setValues] = useState({ title: "", isDone: false });
+  const handleChange = (event) => {
+    const value = event.target.value;
+    const newObj = { ...values };
+    newObj.title = value;
+    setValues(newObj);
+  };
   return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
+    <div className="body-container">
+      <header>
+        <h1>ToDo List</h1>
+      </header>
+      <main>
+        {/* Cette input permet de récuperer la task écrite */}
+        <input
+          placeholder="new task"
+          type="text"
+          onChange={handleChange}
+        ></input>
+        {/* Ce bouton permet de rajouter la task dans une liste*/}
+        <button
+          onClick={() => {
+            console.log(values);
+          }}
+        >
+          Add task
+        </button>
+      </main>
     </div>
   );
 }
